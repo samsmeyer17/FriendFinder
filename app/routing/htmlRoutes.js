9ee74f6e55ca40ea18599a8f6d4601b5ec76041a
Samuel Meyer <sam.smeyer17@gmail.com>
2019-10-21 22:14:15 -0700

    adding api and html routes as well as adding questions and answer choices

---
+++ app/routing/htmlRoutes.js
@@ -0,0 +1,11 @@
+var path = require("path");
+
+module.exports = function (app) {
+  app.get("/survey", function (req, res) {
+    res.sendFile(path.join(__dirname, "../public/survey.html"))
+  });
+
+  app.get("/", function (reg, res) {
+    res.sendFile(path.join(__dirname, "../public/home.html"))
+  })
+}
